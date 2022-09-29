18a2b03e73dc6b08091ea877c573917371971b7d
Umais <mianumais1997@gmail.com>
2022-09-29 15:31:00 +0500

    alarm notification library

---
+++ android/build/generated/source/buildConfig/release/com/emekalites/react/alarm/notification/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.emekalites.react.alarm.notification;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.emekalites.react.alarm.notification";
+  public static final String BUILD_TYPE = "release";
+}
